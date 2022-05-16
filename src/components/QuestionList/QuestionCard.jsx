9c85ab55d2fec533b8b85bda806154e0b01ff851
maxmay94 <maxmay94@gmail.com>
2022-05-16 17:57:07 -0400

    clicking on value brings up question then answer then blank

--- src/components/QuestionList/QuestionCard.jsx
+++ src/components/QuestionList/QuestionCard.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const QuestionCard = (props) => {
+
+  const[questionPicked, setQuestionPicked] = useState(0)
+
+  useEffect(() => {
+    setQuestionPicked(0)
+  },[])
+
+  console.log(questionPicked)
+
+  const clickQuestion = () => {
+    setQuestionPicked(questionPicked + 1)
+  }
+
   return(
-    <div className='bg-blue-900 rounded-sm text-center h-full drop-shadow-lg'>
-        <button className='h-full w-full border-y-2 border-y-black'>
-          <h1 className='text-yellow-600 font-black text-xl md:text-2xl lg:text-4xl xl:text-6xl drop-shadow-lg'>${props.question.difficulty}</h1>
-          {/* <h1>Question: {props.question.question}</h1>
-          <h1>Answer: {props.question.answer}</h1> */}
-          <br />
-        </button>   
-    </div>
+    <>
+    {
+      questionPicked === 0 ?
+
+        <div className='rounded-sm text-center h-full drop-shadow-lg'>
+            <button className='h-full w-full border-y-2 border-y-black' onClick={clickQuestion}>
+              <h1 className='text-yellow-500 font-black text-xl md:text-2xl lg:text-4xl xl:text-6xl drop-shadow-lg'>${props.question.difficulty}</h1>
+              {/* <h1>Question: {props.question.question}</h1>
+              <h1>Answer: {props.question.answer}</h1> */}
+              <br />
+            </button>   
+        </div>
+      :
+
+      questionPicked === 1 ?
+        <div className='text-white font-black text-2xl rounded-sm text-center h-full drop-shadow-lg'>
+          <button className='h-full w-full border-y-2 border-y-black'  onClick={clickQuestion}>
+            <h1>{props.question.question}</h1>
+          </button>
+        </div>
+
+      :
+      questionPicked === 2 ?
+        <div className='text-white font-black text-3xl rounded-sm text-center h-full drop-shadow-lg' >
+          <button  className='h-full w-full border-y-2 border-y-black' onClick={clickQuestion}>
+            <h1>{props.question.answer}</h1>
+          </button>
+        </div>
+      :
+
+        <div className='rounded-sm text-center h-full drop-shadow-lg border-y-2 border-y-black'>
+
+        </div>
+    }
+    </>
   )
 }
 
